247b654caabef1e6deb32690a5f879af5c934620
johanh <johan.hagel@precisebiometrics.com>
2017-11-25 16:26:04 +0100

    hej

--- test1.c
+++ test1.c
@@ -5,4 +5,9 @@ Skriver lite mer här nu, det får bli tillägg 1.
 
 Fortsätter, här kommer tillägg 2.
 
+Nu tillägg3, men det ska jag inte adda innna jag göra commit.
+Så det kommer inte vara med i commiten.
+
+Däremot i nästa commit. Men så vill jag bara skicka in föregående commit... 
+
 "
